50e84ee407529017f042cdcfde19dbeca7fc10bd
Narayan Thakare <narayan111thakare@gmail.com>
2023-06-23 12:25:55 +0530

    second commit

--- src/controller/LIKEOPERATOR.java
+++ src/controller/LIKEOPERATOR.java
@@ -1,10 +1,27 @@
 package controller;
 
+import java.sql.*;
+import java.util.Scanner;
+
 public class LIKEOPERATOR {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/pratice";
+        String username = "root";
+        String password = "abc123";
+        Scanner s=new Scanner(System.in) ;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con=DriverManager.getConnection(url,username,password);
+        
+        System.out.println("Enter the name");
+String name=s.nextLine();
+             ResultSet rs=null;
+ String sql = "SELECT * FROM pratice.pra WHERE name  like  '" + name + "%'";
+	PreparedStatement stmt=con.prepareStatement(sql);
+rs=stmt.executeQuery();
+while(rs.next()){
+	System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+}
+        		
+        		
+    }
 }
